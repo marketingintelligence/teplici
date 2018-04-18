e321f1b6686c31b26cf3057a5b347c1a50fe2950
Damir Savezhanov <savezhanov@gmail.com>
2018-04-18 09:54:28 +0600

    languages folder

---
+++ languages/eng.php
@@ -0,0 +1,70 @@
+﻿<?php
+    //header
+    $arraylang["home"] = "Home";
+    $arraylang["about"] = "About";
+    $arraylang["exhibition"] = "Exhibition";
+    $arraylang["association"] = "Association";
+    $arraylang["newsheader"] = "News";
+    $arraylang["art_pub"] = "Articles and publications";
+    $arraylang["send"] = "SEND";
+    $arraylang["request"] = "Request for a greenhouse";
+
+    //slider and news
+    //Used *news,read
+    $arraylang["our"] = "Our";
+    $arraylang["partners"] = "Partners";
+
+    //footer
+    //used *send
+    $arraylang["leave_message"] = "Leave a message";
+    $arraylang["contact_with"] = "And we will contact you!";
+    $arraylang["leave_message_btn"] = "Leave a message";
+    $arraylang["success_message"] = "Thank you! Your message has been sent!";
+    $arraylang["name"] = "Name";
+    $arraylang["message"] = "Message";
+
+    //articles
+    //used *read
+    $arraylang["states"] = "ARTICLES";
+    $arraylang["and"] = "and";
+    $arraylang["publications"] = "PUBLICATIONS";
+    $arraylang["comp"] = "of company";
+
+    //news page
+    $arraylang["news"] = "Company";
+    $arraylang["company"] = "news";
+    $arraylang["read"] = "Read more";
+
+    //anketa
+    $arraylang["questionnaire"] = "QUESTIONNAIRE";
+    $arraylang["tech_tasks"] = "to prepare a technical task";
+    $arraylang["thanks"] = "Thank you!!!";
+    $arraylang["q_success"] = "Your form has been sent.";
+    $arraylang["data"] = "Information about the company";
+    $arraylang["required"] = "required fields.";
+    $arraylang["nomination"] = "nomination";
+    $arraylang["form_self"] = "Type of ownership";
+
+    //association
+    $arraylang["head_title"] = "We are working for prosperity of";
+    $arraylang["head_title2"] = "Kazakhstan's agriculture!";
+    $arraylang["greenhouse_ass"] = "ASSOCIATION OF GREENHOUSES";
+    $arraylang["kaz"] = "OF KAZAKHSTAN";
+    $arraylang["hothouse"] = "Hothouse";
+    $arraylang["combines"] = "combines";
+    $arraylang["supplier"] = "Suppliers of";
+    $arraylang["hot_tech"] = "greenhouse technology";
+    $arraylang["participate"] = "Participate in our annual";
+    $arraylang["participate2"] = "international exhibition!";
+    $arraylang["more"] = "More info";
+
+    //about
+    $arraylang["about_comp"] = "About our";
+    $arraylang["comp_about"] = "company";
+
+    //home
+    $arraylang["map"] = "Map of";
+    $arraylang["main_hot"] = "greenhouses";
+    $arraylang["main_kaz"] = "in Kazakhstan";
+?>
+
---
+++ languages/ru.php
@@ -0,0 +1,71 @@
+<?php
+    //header
+    $arraylang["home"] = "Главная";
+    $arraylang["about"] = "О нас";
+    $arraylang["exhibition"] = "Выставки";
+    $arraylang["association"] = "Ассоциация";
+    $arraylang["newsheader"] = "Новости";
+    $arraylang["art_pub"] = "Статьи  и  публикации";
+    $arraylang["send"] = "ОТПРАВИТЬ";
+    $arraylang["request"] = "Нужна теплица?";
+
+    //slider and news
+    //used news,read
+    $arraylang["our"] = "Наши";
+    $arraylang["partners"] = "Партнеры";
+
+    //footer
+    //used send
+    $arraylang["leave_message"] = "Оставьте сообщение";
+    $arraylang["contact_with"] = "И мы свяжемся с Вами!";
+    $arraylang["leave_message_btn"] = "Оставить сообщение";
+    $arraylang["success_message"] = "Спасибо! Ваше сообщение отправлено!";
+    $arraylang["name"] = "Имя";
+    $arraylang["message"] = "Сообщение";
+
+    //articles
+    //used *read
+    $arraylang["states"] = "СТАТЬИ";
+    $arraylang["and"] = "и";
+    $arraylang["publications"] = "ПУБЛИКАЦИИ";
+    $arraylang["comp"] = "компании";
+
+    //news page
+    $arraylang["news"] = "Новости";
+    $arraylang["company"] = "компании";
+    $arraylang["read"] = "Читать дальше";
+
+    //anketa
+    $arraylang["questionnaire"] = "АНКЕТА";
+    $arraylang["tech_tasks"] = "для подготовки технического задания";
+    $arraylang["thanks"] = "Спасибо!!!";
+    $arraylang["q_success"] = "Ваша анкета отправлена.";
+    $arraylang["data"] = "Данные о фирме";
+    $arraylang["required"] = "поля, обязательные для заполнения.";
+    $arraylang["nomination"] = "Наименование";
+    $arraylang["form_self"] = "Форма собственности";
+
+    //association
+    $arraylang["head_title"] = "Мы трудимся ради процветания";
+    $arraylang["head_title2"] = "сельского хозяйства Казахстана!";
+    $arraylang["greenhouse_ass"] = "АССОЦИАЦИЯ ТЕПЛИЦ";
+    $arraylang["kaz"] = "КАЗАХСТАНА";
+    $arraylang["hothouse"] = "Тепличные";
+    $arraylang["combines"] = "комбинаты";
+    $arraylang["supplier"] = "Поставщики";
+    $arraylang["hot_tech"] = "тепличной технологии";
+    $arraylang["participate"] = "Участвуйте в нашей ежегодной";
+    $arraylang["participate2"] = "международной выставке!";
+    $arraylang["more"] = "Подробнее";
+
+    //about
+    //used
+    $arraylang["about_comp"] = "О Нашей";
+    $arraylang["comp_about"] = "компании";
+
+    //home
+    $arraylang["map"] = "Карта";
+    $arraylang["main_hot"] = "Теплиц";
+    $arraylang["main_kaz"] = "Казахстана";
+?>
+
